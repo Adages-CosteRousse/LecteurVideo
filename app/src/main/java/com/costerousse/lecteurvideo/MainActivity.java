0d3dc2e551eaafc8a646d19db351d0cd2cdd2310
Hamoth <alexis.japas@protonmail.com>
2019-07-29 15:58:29 +0200

    ConstraintLayout -> LinearLayout

--- app/src/main/java/com/costerousse/lecteurvideo/MainActivity.java
+++ app/src/main/java/com/costerousse/lecteurvideo/MainActivity.java
@@ -1,16 +1,21 @@
 package com.costerousse.lecteurvideo;
 
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
+	// Attributs de la classe
 	
+	
+	// Méthodes de la classe
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
 	}
 }
